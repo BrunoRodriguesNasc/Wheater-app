222b3ead36520f89e23a68915d49f504ee643419
bruno-ctrl <https://github.com/bruno-ctrl>
2021-08-24 12:22:49 -0300

    removendo componente indevido

--- frontend/src/components/sideContainer/menuModal/Modal.js
+++ frontend/src/components/sideContainer/menuModal/Modal.js
@@ -1,24 +1,18 @@
-import React,{forwardRef, useRef} from "react";
+import React, { useRef } from "react";
 import Form from "./Form";
 import Button from "./Button";
 import Select from "./Select";
 
+export default function Modal({ getName, closeModal }) {
+  const divModal = useRef();
 
-export default function  Modal({ getName, closeModal }) {
-  const divModal = useRef(HTMLDivElement)
-  
-  forwardRef
-  const getNameLocation = (e,value) => {
+  const getNameLocation = (e, value) => {
     e.preventDefault();
     getName(value);
-    divModal.current?.classList.remove('active')
-  
+    divModal.current?.classList.remove("active");
   };
   return (
-    <div
-    className="modal"
-    ref={divModal}
-    >
+    <div className="modal" ref={divModal}>
       <div className="div-close">
         <Button nameClass="button-close" toDoFunction={closeModal}>
           X

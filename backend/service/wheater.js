eed930d7551300cfd883a15bd924a4b11a8f66fa
Bruno Rodrigues <bruno_rodrigues977@hotmail.com>
2022-07-23 11:10:43 -0300

    tirando callbacks da funcção

--- backend/service/wheater.js
+++ backend/service/wheater.js
@@ -3,18 +3,8 @@ const { URL_GET_WHEATER_CITY } = require("../constants/const");
 
 const getCity = async (city, days) => {
   const url = `${URL_GET_WHEATER_CITY}${city}&days=${days}&aqi=no&alerts=no`;
-    const response = await axios
-    .get(url)
-    .then(function (response) {
-      return response.data;
-    })
-    .catch(function (error) {
-      if (error) {
-        console.log(error);
-      }
-    });
-    console.log(response);
-    return response;
+    const { data } = await axios.get(url);
+    return data;
 }
 
 module.exports = getCity;

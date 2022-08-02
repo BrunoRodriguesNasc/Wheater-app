f5bbeebd17f891b4c6067ff1160262891283766b
Bruno Rodrigues <bruno_rodrigues977@hotmail.com>
2022-08-02 20:09:07 -0300

    Refactor

--- backend/server/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const wheater = require('../router/wheater.js');
-const bodyParser = require("body-parser");
-
-app.use('/wheater', wheater);
-app.use(cors());
-app.listen(3333);
